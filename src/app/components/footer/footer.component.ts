b83ed845b99825bd9b8869a599378a493462a1e0
Diego Rocha <drochaesc@gmail.com>
2023-10-20 21:50:46 -0600

    Refactoring project scope and footer design

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  public isCompanyExpanded = false;
+  public isSocialsExpanded = false;
+  public isOtherExpanded = false;
 
+  ngOnInit(): void { // if it is a large device, start already expanded lists
+    if (window.innerWidth >= 640) {
+      this.isCompanyExpanded = true;
+      this.isSocialsExpanded = true;
+      this.isOtherExpanded = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth >= 640) {
+      this.isCompanyExpanded = true;
+      this.isSocialsExpanded = true;
+      this.isOtherExpanded = true;
+    }
+    if (event.target.innerWidth < 640) {
+      this.isCompanyExpanded = false;
+      this.isSocialsExpanded = false;
+      this.isOtherExpanded = false;
+    }
+  }
+
+  toggleCompanyList() {
+    if (window.innerWidth >= 640) { //Expand the list for big devices.
+      this.isCompanyExpanded = true;
+    } else {
+      this.isCompanyExpanded = !this.isCompanyExpanded;
+    }
+  }
+
+  toggleSocialsList() {
+    if (window.innerWidth >= 640) { //Expand the list for big devices.
+      this.isSocialsExpanded = true;
+    } else {
+      this.isSocialsExpanded = !this.isSocialsExpanded;
+    }
+  }
+
+  toggleOtherList() {
+    if (window.innerWidth >= 640) { //Expand the list for big devices.
+      this.isOtherExpanded = true;
+    } else {
+      this.isOtherExpanded = !this.isOtherExpanded;
+    }
+  }
 }
